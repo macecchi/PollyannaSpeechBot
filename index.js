1761879658aede1fab0572f34544f946a80ec963
Mario Cecchi <macecchi@gmail.com>
2016-12-05 00:41:08 -0200

    Initial working version

---
+++ index.js
@@ -0,0 +1,62 @@
+var AWS = require('aws-sdk');
+var TelegramBot = require('node-telegram-bot-api');
+var uuid = require('node-uuid');
+
+AWS.config.loadFromPath('./config.json');
+
+var bot = new TelegramBot(process.env.MERIW_BOT_TELEGRAM_TOKEN, {polling: true});
+let Polly = new AWS.Polly();
+
+var voiceForChat = {};
+
+bot.on('message', (msg) => {
+	if (msg.entities) return;
+
+  var chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'To convert to speech use /s text. To change the voice, use /v voice.');
+});
+
+
+// Matches "/s [whatever]"
+bot.onText(/\/s (.+)/, (msg, match) => {
+  var chatId = msg.chat.id;
+  var voice = voiceForChat[chatId] || 'Vitoria';
+  var text = match[1];
+
+  console.log(`Chat ${chatId} requested "${text}"`);
+
+	let params = {
+		Text: text,
+		VoiceId: voice,
+		OutputFormat: 'mp3'
+	};
+
+	let responseParams = {
+		caption: text
+	}
+
+	Polly.synthesizeSpeech(params, (err, data) => {
+		if (err || data == null) {
+			console.log(err);
+			return;
+		}
+
+		bot.sendVoice(chatId, data.AudioStream, responseParams);
+
+  	console.log(`Chat ${chatId} received voice for "${text}"`);
+	});
+
+});
+
+
+bot.onText(/\/v (.+)/, (msg, match) => {
+  var chatId = msg.chat.id;
+  let voice = match[1];
+  if (voice != 'Vitoria' && voice != 'Ricardo') {
+  	return bot.sendMessage(chatId, 'Unsupported voice.');
+  }
+
+  voiceForChat[chatId] = voice;
+	bot.sendMessage(chatId, 'Done!');
+	console.log(`Chat ${chatId} changed voice to "${voice}".`);
+});
