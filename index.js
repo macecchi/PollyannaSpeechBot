c9af395df8614e2878ab4271fea8c6c0ab8b479e
Mario Cecchi <macecchi@gmail.com>
2016-12-13 18:24:27 -0200

    List all available choices from AWS Polly

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ Bot.on('message', (msg) => {
   if (msg.entities) return;
 
   let chatId = msg.chat.id;
-  Bot.sendMessage(chatId, 'To convert to speech use /s text. To change the voice, use /v voice.');
+  Bot.sendMessage(chatId, 'To convert to speech use /s text. To check the available voices, use /voices.');
 });
 
 
@@ -36,18 +36,42 @@ Bot.onText(/\/s (.+)/, (msg, match) => {
 
     Bot.sendVoice(chatId, audioStream, responseParams);
   });
-
 });
-
 
 Bot.onText(/\/v (.+)/, (msg, match) => {
   let chatId = msg.chat.id;
-  let voice = match[1];
-  if (voice != 'Vitoria' && voice != 'Ricardo') {
-    return Bot.sendMessage(chatId, 'Unsupported voice. Supported voices:\nVitoria\nRicardo');
+  let requestedVoice = match[1];
+
+  let voices = Polly.availableVoices || [];
+  for (let voice of voices) {
+    if (voice.Name.toLowerCase() == requestedVoice.toLowerCase()) {
+      voiceForChat[chatId] = voice.Name;
+      Bot.sendMessage(chatId, `Done! You are now using ${voice.Name}.`);
+      console.log(`Chat ${chatId} changed voice to ${voice.Name}`);
+      return;
+    }
   }
 
-  voiceForChat[chatId] = voice;
-  Bot.sendMessage(chatId, 'Done!');
-  console.log(`Chat ${chatId} changed voice to "${voice}"`);
+  Bot.sendMessage(chatId, 'Unsupported voice. Type /voices to get the list of available voices.');
+  console.log(`Chat ${chatId} tried to change voice to "${requestedVoice}" (invalid)`);  
+});
+
+
+Bot.onText(/\/voices/, (msg, match) => {
+  let chatId = msg.chat.id;
+
+  console.log(`Chat ${chatId} requested the list of voices`);
+
+  let voices = Polly.availableVoices;
+  if (Array.isArray(voices)) {
+    let responseText = 'Available voices:\n';
+    for (let voice of voices) {
+      responseText += `- ${voice.Name} (${voice.LanguageCode})\n`;
+    }
+
+    responseText += `\nTo change the voice, enter /v and the name of the voice.`;
+    Bot.sendMessage(chatId, responseText);
+  } else {
+    console.log(`Voices unavailable.`);
+  }
 });
--- polly.js
+++ polly.js
@@ -4,6 +4,7 @@ class PollyClient {
 	constructor() {
 		AWS.config.loadFromPath('./config.json');
 		this.polly = new AWS.Polly();
+    this.updateAvailableVoices();
 	}
 
 	synthesizeText(text, voice, callback) {
@@ -22,6 +23,19 @@ class PollyClient {
       callback(null, data.AudioStream);
     });
 	}
+
+  updateAvailableVoices() {
+    let _this = this;
+    this.polly.describeVoices({}, (err, data) => {
+      if (err || data == null) {
+        console.err('Polly: unable to update the list of available voices.');
+        return;
+      }
+
+      _this.availableVoices = data.Voices;
+      console.log('Polly: updated the list of available voices.', _this.availableVoices);
+    });
+  }
 }
 
 module.exports = PollyClient;
